a4e9c93428574ed7b8e7ccbaae3be53c7e259491
Cesar <Cesarsiri@live.com>
2023-03-28 20:01:25 -0700

    commited by cesar

--- gameClass/shields.js
+++ gameClass/shields.js
@@ -10,17 +10,29 @@ this.velocity = {
 }
 this.width = 960;
 this.height = 576;
+this.sprite_right = 0;
+this.sprite_bottom = 0;
 }
 // shield animation
 drawShield(){
     const shieldImage = new Image();
 shieldImage.src = "assets/shields/bluebean.png";
-ctx.globalAlpha = 0.2;
-var sprite_right = 4;
-var sprite_bottom = 2;
+ctx.globalAlpha = 1;
 
+this.sprite_right ++
+if(this.sprite_right == 6){
+    this.sprite_right = 0
+   // this.sprite_bottom ++
+}
+// if(this.sprite_bottom == 3){
+//     this.sprite_bottom = 0;
+//     this.sprite_right = 0;
+// }
 
-ctx.drawImage(shieldImage, 1 * (192 * 0), 1 * (192 * 0), this.width, this.height, player.position.x - 45 - (192 * 3), player.position.y - 40 - (192 * 0), this.width, this.height );
+console.log("bottom" + this.sprite_bottom);
+console.log( "right" + this.sprite_right);
+
+ctx.drawImage(shieldImage, 1 * (192 * 4), 1 * (192 * 2), this.width, this.height, player.position.x - 45 - (192 * this.sprite_right), player.position.y - 40 - (192 *  this.sprite_bottom), this.width, this.height );
 ctx.globalAlpha = 1;
 }
 updateshield(){
