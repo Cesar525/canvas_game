66f8ac38cf84442c42ae3909d89abd42985abe01
Cesar Calderon <cesarsiri@live.com>
2023-05-01 21:53:12 -0400

    commiteD

--- gameClass/functions.js
+++ gameClass/functions.js
@@ -47,25 +47,25 @@ function collisionMonster(monsters, player, shot){
         
     }
 
-   // bullet disapear collision
+   //bullet disapear collision
     shot.counter += 1;
     if(shot.counter > 5 && shot.clearRect){
-        //console.log("reset");
+        console.log("reset");
         shot.clearRect = false;
      
    
     }
-    //player collision
+   // player collision
     if(collision.collisionTouch(player, monsters)){
         player.body.health -= 1; 
         console.log("you have been damage your current life is  = " + player.body.health);
         
     }
     
-    //console.log(exp);
+  
 if(collision.collisionTouch(shot, monsters)){
 shot.clearRect = true;
-//console.log("Bullet banished..");
+console.log("Bullet banished..");
 shot.position.x = -50;
 shot.position.y = - 50;
 }  
