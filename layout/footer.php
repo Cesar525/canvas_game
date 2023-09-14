bbd04a9c2f83387b41bc3249d2351cfe7a223565
Cesar Calderon <cesarsiri@live.com>
2023-09-13 21:16:29 -0400

    commited by cesar

--- layout/footer.php
+++ layout/footer.php
@@ -16,10 +16,3 @@
 
 
 
-</div> <!--body_chest end -->
-
-<div class="body_footer"><font>These are my feet</font></div>
-
-
-</body>
-</html>
--- layout/header.php
+++ layout/header.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html>
 
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Developments">
-    <meta name="keywords" content="HTML, CSS, JavaScript">
-    <meta name="author" content="Cesar">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Web tittle</title>
-    <!-- <link rel="ikcon" type="image/x-icon" href="layout/img/logo.png"> -->
-    <link rel="stylesheet" href="layout/style.css">
-    <link rel="shortcut icon" href="#">
-
-    
 
 <body>
 
