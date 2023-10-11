ceb7447e6cd10441f199d66852187b5f14dfd1c7
Cesar Calderon <cesarsiri@live.com>
2023-10-10 22:08:20 -0400

    commited by cesar

--- game.js
+++ game.js
@@ -45,7 +45,7 @@ const animation_TESTING = new Animation();
 //players
 //CREATING A PLAYER Player(name, level, thruster_selection, get_health, energy, m_damage,  gunType, posx, posy, gun_speed)
 const players = [
-  new Player("PLAYER ONE", 1230,  8, 2000, 400000, 5000, 20, 100, 900, 30),
+  new Player("PLAYER ONE", 1230,  8, 2000, 400000, 500, 1, 100, 900, 30),
   // new Player("PLAYER TWO", 1230,  8, 2000, 400000, 5000, 20, 300, 900, 30),
   // new Player("PLAYER TWO", 1230,  8, 2000, 400000, 5000, 20, 600, 900, 30),
   // new Player("PLAYER TWO", 1230,  8, 2000, 400000, 5000, 20, 900, 900, 30),
